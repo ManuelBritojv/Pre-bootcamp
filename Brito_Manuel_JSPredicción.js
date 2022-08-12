7b781d1032ead2e1c0ca11db1316d9ab0601c77f
ManuelBritojv <manuelbritojv@gmail.com>
2022-08-12 12:36:56 -0400

    Prediccion

--- Brito_Manuel_JSPredicción.js
+++ Brito_Manuel_JSPredicción.js
@@ -5,7 +5,7 @@ function myBirthYearFunc(){
             console.log("Nací en " + EntradaAñoNacimiento); // Si EntadaAñoNacimiento == 1980, el console.log sería "Nací en 1980" 
         }
         function add(num1, num2){
-            console.log("¡Sumando números!"); // el console.log sería ¡Sumando números!, num1 is: 10, num2 is: 20
+            console.log("¡Sumando números!"); // el console.log sería ¡Sumando números!, num1 is: 10, num2 is: 20, 30
             console.log("num1 is: " + num1);
             console.log("num2 is: " + num2);
             var sum = num1 + num2;
