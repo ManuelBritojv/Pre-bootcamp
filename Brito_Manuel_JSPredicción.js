0a3f80e4133a8d9daca336b8bd5933d62ec988ca
ManuelBritojv <manuelbritojv@gmail.com>
2022-08-12 12:43:16 -0400

    Prediccion

--- Brito_Manuel_JSPredicción.js
+++ Brito_Manuel_JSPredicción.js
@@ -1,14 +1,14 @@
 function myBirthYearFunc(){
         console.log("Nací en " + 1980); //Lo que indicara el console.log es el año de nacimiento agregando un "Nací en" antes.
     }
-    function myBirthYearFunc(EntradaAñoNacimiento){
-            console.log("Nací en " + EntradaAñoNacimiento); // Si EntadaAñoNacimiento == 1980, el console.log sería "Nací en 1980" 
-        }
-        function add(num1, num2){
-            console.log("¡Sumando números!"); // el console.log sería ¡Sumando números!, num1 is: 10, num2 is: 20, 30
-            console.log("num1 is: " + num1);
-            console.log("num2 is: " + num2);
-            var sum = num1 + num2;
-            console.log(sum);
-        }
+function myBirthYearFunc(EntradaAñoNacimiento){
+    console.log("Nací en " + EntradaAñoNacimiento); // Si EntadaAñoNacimiento == 1980, el console.log sería "Nací en 1980", pero como no es el caso seria Nací en undefined
+}
+function add(num1, num2){
+    console.log("¡Sumando números!"); // el console.log sería ¡Sumando números!, num1 is: 10, num2 is: 20, 30
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
                         
